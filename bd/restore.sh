d42560f0407c3e1e6039956dc323b208ee061ae2
Bruno <bruno@iessanclemente.net>
2024-02-20 12:25:33 +0100

    variables de entorno

--- bd/restore.sh
+++ bd/restore.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker compose exec basedatos /bin/bash -c "mariadb -u user -puser bd < /bd/backup.sql"
+docker compose exec basedatos /bin/bash -c "mariadb -u user -puser db < /bd/backup.sql"
